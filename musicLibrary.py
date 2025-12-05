ca1a1e7ed06f960dd952f48c71ea279c75a662a9
amol_darkunde <amoldarkunde014@gmail.com>
2025-12-05 22:30:17 +0530

    Loma - Virtual Assistant

---
+++ musicLibrary.py
@@ -0,0 +1,10 @@
+lmusic = {
+    "espresso": "https://www.youtube.com/watch?v=eVli-tstM5E&list=RDeVli-tstM5E&start_radio=1",
+    "taste": "https://www.youtube.com/watch?v=KEG7b851Ric&list=RDKEG7b851Ric&start_radio=1",
+    "juno": "https://www.youtube.com/watch?v=1aMCVgRKcP4&list=RD1aMCVgRKcP4&start_radio=1",
+    "please_please_please": "https://www.youtube.com/watch?v=cF1Na4AIecM&list=RDcF1Na4AIecM&start_radio=1",
+    "love_me": "https://www.youtube.com/watch?v=KY44zvhWhp4&list=RDKY44zvhWhp4&start_radio=1",
+    "tears": "https://www.youtube.com/watch?v=V9vuCByb6js&list=RDV9vuCByb6js&start_radio=1",
+    "when_did_you_get_hot": "https://www.youtube.com/watch?v=bnhV-OBnGCE&list=RDbnhV-OBnGCE&start_radio=1",
+    "birds_of_a_feather": "https://www.youtube.com/watch?v=V9PVRfjEBTI&list=RDV9PVRfjEBTI&start_radio=1"
+}
